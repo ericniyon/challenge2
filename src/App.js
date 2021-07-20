b569dda8c93d34926e0d3d158cebd29bc61b5c0a
Eric <niyoeri6@gmail.com>
2021-07-20 14:31:43 +0200

    searching album photos using react js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
+import {Fragment, useState, useEffect } from 'react';
 import './App.css';
 
+// const url = ;
+
 function App() {
+    const [albumId, setAlbumID] = useState(0)
+    const [photos, setPhotos] = useState([]);
+
+  const getUser = async (id) => {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/albums/${id}/photos/`
+    );
+    const data = await response.json()
+    setPhotos(data)
+    console.log(photos.length)
+  }
+
+
+  useEffect(()=> {
+    getUser()
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Fragment>
+      <div className='d-flex justify-content-center mb-5'>
+        <div className='mt-5 '>
+          <input
+            type='number'
+            value={albumId}
+            onChange={(e) => setAlbumID(e.target.value)}
+            className='form-control'
+          />
+        </div>
+        <button
+          type='submit'
+          onClick={(e) => getUser(albumId)}
+          className='btn btn-info btn-md mt-5 ml-2'
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Get Album Photos By Id
+        </button>
+        <div className='App'></div>
+      </div>
+      <div className='container-fluid'>
+        <div className='row'>
+          {photos.length > 0 ? (
+            photos.map((photo) => (
+              <div className='col-md-3 col-sm-6'>
+                <div className='card m-2'>
+                  <img
+                    src={photo.thumbnailUrl}
+                    className='card-img-top'
+                    alt='album photo '
+                  />
+                  <div className='card-body'>
+                    <h5 className='card-title'>{photo.title}</h5>
+                  </div>
+                </div>
+              </div>
+            ))
+          ) : (
+            <div className="col-md-4 offset-2">
+
+            <p className='d-inline-block text-nowrap '>
+              Enter album id and click "Get Album Photos By Id button" to see
+              your photos
+            </p>
+            </div>
+          )}
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
